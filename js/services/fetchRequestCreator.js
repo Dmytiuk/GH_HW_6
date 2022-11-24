7bfc39fe8fedff0cb17157cc22b80d8ec04fab95
Valentin <valaimg2218@gmail.com>
2022-11-24 10:10:45 +0200

    removed token

--- js/services/fetchRequestCreator.js
+++ js/services/fetchRequestCreator.js
@@ -1,8 +1,5 @@
 export const fetchRequestCreator = async url => {
     return fetch(url, {
                 method: 'GET',
-                headers: {
-                    'Authorization': 'token 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',
-                },
             });
 }
