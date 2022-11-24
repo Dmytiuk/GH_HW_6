141c1bcc7b64ffda1fd33402b88a918b42e7fea9
Valentin <valaimg2218@gmail.com>
2022-11-24 10:06:22 +0200

    new token

--- js/services/fetchRequestCreator.js
+++ js/services/fetchRequestCreator.js
@@ -2,7 +2,7 @@ export const fetchRequestCreator = async url => {
     return fetch(url, {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'token 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',
+                    'Authorization': 'token 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',
                 },
             });
 }
